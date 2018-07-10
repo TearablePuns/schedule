1f81d56eb333fdd17f96d63576a2e9346b362d0e
TearablePuns <jmemmott13@gmail.com>
2018-07-10 10:01:26 -0600

    Fixed grid and added arrow component

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,23 +1,30 @@
-import React, { Component } from 'react';
-import Icon from '../icon';
+import React, { Component } from "react";
+
+import Icon from "../icon";
+import Arrow from "../arrow";
 
 class LibraryCourse extends Component {
-    render() {
-        return (
-            <div className="library-course">
-                <label className="library-course__title">Problem Solving</label>
-                { Icon('fas fa-check', 'library-course__icon') }
-                {/* icon component */}
-                {/* arrow component */}
-                {/* action button component */}
-                { Icon('fas fa-plus-circle', 'library-course__action')}
-                <div className="library-course__description">
-                    <label>Course Description</label>
-                    <p>There are a bunch of random words in this sentence and I am just typing so I can fulfill a random number of words and I will keep on typing and typing and typing because there needs to be a lot of words here.</p>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="library-course">
+        <div className="library-course__title-arrow">
+          <label className="library-course__title">Problem Solving</label>
+          {Icon("fas fa-check", "library-course__icon")}
+        </div>
+        <Arrow className="library-course__arrow" />
+        {/* action button component */}
+        <div className="library-course__description">
+          <label>Course Description</label>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+            fringilla facilisis mi, non pellentesque metus consectetur vel.
+            Quisque dictum, lectus vitae dignissim tincidunt, sapien nibh
+            placerat diam, quis blandit enim nulla in felis.
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default LibraryCourse;
