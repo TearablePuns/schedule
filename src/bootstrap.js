72fe31c18f388e7597de2222d457e5152a9a6320
TearablePuns <jmemmott13@gmail.com>
2018-07-10 12:10:04 -0600

    installed and connected redux devtools to application and used spread operator to set payload data in store

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
 import Home from './components/home';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware()(compose(window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore));
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './style/main.scss';
 
 function main() {
   ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
+    <Provider store={createStoreWithMiddleware(reducers)}> 
       <Home />
     </Provider>
     , document.querySelector('.app-wrapper'));
