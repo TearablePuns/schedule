71a89ea05739ddbd07aa486f53b9c663c4798ee0
TearablePuns <jmemmott13@gmail.com>
2018-07-12 10:18:19 -0600

    add remove course actions

--- src/components/action.js
+++ src/components/action.js
@@ -1,28 +1,38 @@
-import React, { Component } from 'react';
-import Icon from './icon';
+import React, { Component } from "react";
+import Icon from "./icon";
 
 class Action extends Component {
+  constructor(props) {
+    super(props);
 
-    handleAction = function () {
-        this.props.onClick()
-
-        // document.getElementById('action').classList.add('')
-    }.bind(this)
-    render() {
-        return (
-            <a
-                id='action'
-                onClick={() => this.handleAction()}
-                className={`${this.props.className} action`}>
-            </a>
-        )
-        //     return (
-        //         <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
-        //             {Icon('fas fa-plus-circle')}
-        //             {/* fas fa-times-circle */}
-        //         </button>
-        //     )
+    this.status = false;
+  }
+  handleAction = function() {
+    this.props.onClick();
+    if (!this.status) {
+      document.getElementById(this.id).classList.add("action-remove");
+    } else {
+      document.getElementById(this.id).classList.remove("action-remove");
     }
+
+    this.status = !this.status;
+  }.bind(this);
+  render() {
+      this.id = `action ${this.props.id}`
+    return (
+      <a
+        id={this.id}
+        onClick={() => this.handleAction()}
+        className={`${this.props.className} action`}
+      />
+    );
+    //     return (
+    //         <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
+    //             {Icon('fas fa-plus-circle')}
+    //             {/* fas fa-times-circle */}
+    //         </button>
+    //     )
+  }
 }
 
-export default Action; 
+export default Action;
